a5d059f7b4eb0fa7e73d43b64a1736d1fb077380
heshamnaser93 <heshamnaser93@icloud.com>
2021-09-20 17:59:08 +0300

    fırst-component-added

---
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>september 20th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$300.00</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
