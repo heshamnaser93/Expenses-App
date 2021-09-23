05189fc4b797c7c3f003b99d56c8c6b38abd427d
heshamnaser93 <heshamnaser93@icloud.com>
2021-09-23 18:07:18 +0300

    split-expense-item-to-components

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,10 +1,13 @@
-function ExpenseItem() {
+import ExpenseDate from "../ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <div>september 20th 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$300.00</div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
