d928ef56448e13c9400727389457570fae0f491a
Hesham <heshamnaser93@icloud.com>
2021-10-01 07:43:06 +0300

    oraganize-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
