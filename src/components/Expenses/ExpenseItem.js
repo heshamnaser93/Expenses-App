f69df0166218b5932bd4fb50ea167f9c2f8f7aef
heshamnaser93 <heshamnaser93@icloud.com>
2021-10-28 16:54:12 +0300

    V0.8 adding btn

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,19 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+  const clickHandler = () => {
+    console.log("clicked");
+    title = "updated!";
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button>Change Title</button>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
