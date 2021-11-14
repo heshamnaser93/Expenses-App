9ef4fc8eeb85aefe28808646361a01c0d2ad45cb
Hesham <heshamnaser93@icloud.com>
2021-11-14 17:29:05 +0300

    expenses-demo-app last-commit

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,15 +1,24 @@
 import React from "react";
-import ChartBar from "./ChartBar";
-import "./Chart.css";
+import "./ChartBar.css";
 
-const Chart = () => {
+const ChartBar = (props) => {
+  let barFillHeight = "0%";
+
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
   return (
-    <div className="chart">
-      {props.dataPoints.map((dataPoint) => (
-        <ChartBar value={dataPoint.value} />
-      ))}
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
     </div>
   );
 };
 
-export default Chart;
+export default ChartBar;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -20,6 +21,7 @@ const Expenses = (props) => {
         selectedFilter={filteredYear}
         onFilterChange={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
